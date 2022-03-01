0c7d04cf5ffbc8900e53d97643554c6f5e85edfa
GvinP <92249588+GvinP@users.noreply.github.com>
2022-03-01 11:11:12 +0300

    Sort and Star refactor

--- Sort and Star.js
+++ Sort and Star.js
@@ -1,10 +1,3 @@
 function twoSort(s) {
-    s.sort();
-    let i = s[0];
-    i = i.split('');
-    for (let j = 0; j < i.length-1; j++) {
-        i[j] += "***"
-    }
-    i = i.join("");
-    return i;
+    return s.sort()[0].split('').join('***');
 }
