6bafb335c7ed4ced0e97600b774739d4f1680498
alexey kudriashov <gvinpin72@gmail.com>
2022-12-29 18:49:44 +0300

    Training JS #32: methods of Math---round() ceil() and floor()

---
+++ methods of Math---round() ceil() and floor().js
@@ -0,0 +1,10 @@
+function roundIt(n) {
+  const splittedNumber = n.toString().split(".");
+  if (splittedNumber[0].length > splittedNumber[1].length) {
+    return Math.floor(n);
+  } else if (splittedNumber[0].length < splittedNumber[1].length) {
+    return Math.ceil(n);
+  } else {
+    return Math.round(n);
+  }
+}
