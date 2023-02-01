99b4d575ac8d843da50d2169b1917ceeebafe0af
kudryashovalexey <gvinpin72@gmail.com>
2023-02-01 16:25:21 +0300

    Simple remove duplicates

---
+++ Simple remove duplicates.js
@@ -0,0 +1,7 @@
+function solve(arr) {
+  let newArr = arr.slice().filter((n, i) => arr.indexOf(n) !== i);
+  for (let i = 0; i < newArr.length; i++) {
+    arr.splice(arr.indexOf(newArr[i]), 1);
+  }
+  return arr;
+}
