cbade7860c38a70938f76aef58ca88159c07b8da
kudryashovalexey <gvinpin72@gmail.com>
2023-03-31 20:23:07 +0300

    Polish alphabet

---
+++ Polish alphabet.js
@@ -0,0 +1,18 @@
+const polishLetters = {
+  ą: "a",
+  ć: "c",
+  ę: "e",
+  ł: "l",
+  ń: "n",
+  ó: "o",
+  ś: "s",
+  ź: "z",
+  ż: "z",
+};
+
+function correctPolishLetters(string) {
+  return string
+    .split("")
+    .map((c) => polishLetters[c] || c)
+    .join("");
+}
