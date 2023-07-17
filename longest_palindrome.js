52516caf772ca975ba8e0ed28bf69c5982299d6e
kudryashovalexey <gvinpin72@gmail.com>
2023-07-18 00:41:27 +0500

    longest_palindrome

---
+++ longest_palindrome.js
@@ -0,0 +1,24 @@
+function longestPalindrome(string) {
+  let longestPal = '';
+  const getLongestPalindrome = function (leftPosition, rightPosition) {
+    while (
+      leftPosition >= 0 &&
+      rightPosition < string.length &&
+      string[leftPosition] === string[rightPosition]
+    ) {
+      leftPosition--;
+      rightPosition++;
+    }
+    if (rightPosition - leftPosition > longestPal.length) {
+      longestPal = string.slice(leftPosition + 1, rightPosition);
+    }
+  };
+  for (let i = 0; i < string.length; i++) {
+    getLongestPalindrome(i, i + 1);
+    getLongestPalindrome(i, i);
+    if ((string.length - i) * 2 < longestPal.length) {
+      break;
+    }
+  }
+  return longestPal.length;
+}
