f77ac4b088472b0c3093b6b018436ba963afcdcc
kudryashovalexey <gvinpin72@gmail.com>
2023-07-19 02:19:48 +0500

    Linked Lists - Length & Count

---
+++ Linked Lists - Length & Count.js
@@ -0,0 +1,26 @@
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function length(head) {
+  if (head == null) {
+    return 0;
+  } else {
+    return 1 + length(head.next);
+  }
+}
+
+function count(head, data) {
+  if (head == null) {
+    return 0;
+  }
+  let count = 0;
+  while (head != null) {
+    if (head.data == data) {
+      count++;
+    }
+    head = head.next;
+  }
+  return count;
+}
