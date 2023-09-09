6055764f6b01898ab8c3ced39b1d0f1631425902
kudryashovalexey <gvinpin72@gmail.com>
2023-09-09 23:11:10 +0300

    Initialize my name

--- Initialize my name.js
+++ Initialize my name.js
@@ -1,3 +1,6 @@
-function initializeNames(name){
-    return name.split(' ').map((w,i, arr)=> i === 0 || i === arr.length - 1 ? w : w[0] + '.').join(' ');
+function initializeNames(name) {
+  return name
+    .split(' ')
+    .map((w, i, arr) => (i === 0 || i === arr.length - 1 ? w : w[0] + '.'))
+    .join(' ');
 }
