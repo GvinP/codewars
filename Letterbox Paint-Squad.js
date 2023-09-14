e6855ea44029474139d3406ab44696c9e35d1cb1
kudryashovalexey <gvinpin72@gmail.com>
2023-09-14 17:16:43 +0500

    Letterbox Paint-Squad

---
+++ Letterbox Paint-Squad.js
@@ -0,0 +1,9 @@
+var paintLetterboxes = function (start, end) {
+  let res = Array(10).fill(0);
+  for (let i = start; i <= end; i++) {
+    for (let n of i + '') {
+      res[n]++;
+    }
+  }
+  return res;
+};
