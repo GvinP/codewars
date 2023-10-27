fb66a4308c13f5cc3c7956536fdb2e19aa12c744
kudryashovalexey <gvinpin72@gmail.com>
2023-10-27 21:32:02 +0300

    What's up next?

---
+++ What's up next?.js
@@ -0,0 +1,12 @@
+function nextItem(xs, item) {
+  let found = false;
+  let finalValue;
+  for (const i of xs) {
+    if (found) {
+      finalValue = i;
+      break;
+    }
+    if (i === item) found = true;
+  }
+  return finalValue;
+}
